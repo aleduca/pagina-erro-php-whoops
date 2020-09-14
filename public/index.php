a0e822fd597ae11ec21d5e3fec1c39cf99cf9f8b
aleduca <alecar2007@gmail.com>
2020-09-13 22:42:04 -0300

    Primeiro commit
    Projeto da aula aprendendo a usar o whoops

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+require '../vendor/autoload.php';
+require '../helpers/dotenv.php';
+require '../helpers/whoops.php';
+
+// try {
+//     throw new Exception('Erro');
+// } catch (Exception $e) {
+//     var_dump($e->getMessage());
+// }
+
+echo $a;
